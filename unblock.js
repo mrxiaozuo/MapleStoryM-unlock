3782b8d45716bfef03ff904406b6c74413153528
mrxiaozuo <q405483000@hotmail.com>
2023-03-29 14:27:22 +0800

    Update unblock.js

--- unblock.js
+++ unblock.js
@@ -15,24 +15,140 @@ QuantumultX
 hostname = m-api.nexon.com
 
 */
-let isQuantumultX=undefined===this.$task?false:true;let isSurge=undefined===this.$httpClient?false:true;var $task=isQuantumultX?this.$task:{};var $httpClient=isSurge?this.$httpClient:{};var $prefs=isQuantumultX?this.$prefs:{};var $persistentStore=isSurge?this.$persistentStore:{};var $notify=isQuantumultX?this.$notify:{};var $notification=isSurge?this.$notification:{};if(isQuantumultX){var errorInfo={error:''};$httpClient={get:(url,cb)=>{var urlObj;if(typeof(url)=='string'){urlObj={url:url}}else{urlObj=url}$task.fetch(urlObj).then(response=>{cb(undefined,response,response.body)},reason=>{errorInfo.error=reason.error;cb(errorInfo,response,'')})},post:(url,cb)=>{var urlObj;if(typeof(url)=='string'){urlObj={url:url}}else{urlObj=url}url.method='POST';$task.fetch(urlObj).then(response=>{cb(undefined,response,response.body)},reason=>{errorInfo.error=reason.error;cb(errorInfo,response,'')})}}}if(isSurge){$task={fetch:url=>{return new Promise((resolve,reject)=>{if(url.method=='POST'){$httpClient.post(url,(error,response,data)=>{if(response){response.body=data;resolve(response,{error:error})}else{resolve(null,{error:error})}})}else{$httpClient.get(url,(error,response,data)=>{if(response){response.body=data;resolve(response,{error:error})}else{resolve(null,{error:error})}})}})}}}if(isQuantumultX){$persistentStore={read:key=>{return $prefs.valueForKey(key)},write:(val,key)=>{return $prefs.setValueForKey(val,key)}}}if(isSurge){$prefs={valueForKey:key=>{return $persistentStore.read(key)},setValueForKey:(val,key)=>{return $persistentStore.write(val,key)}}}if(isQuantumultX){$notification={post:(title,subTitle,detail)=>{$notify(title,subTitle,detail)}}}if(isSurge){$notify=function(title,subTitle,detail){$notification.post(title,subTitle,detail)}}
+let isQuantumultX = undefined === this.$task ? false: true;
+let isSurge = undefined === this.$httpClient ? false: true;
+var $task = isQuantumultX ? this.$task: {};
+var $httpClient = isSurge ? this.$httpClient: {};
+var $prefs = isQuantumultX ? this.$prefs: {};
+var $persistentStore = isSurge ? this.$persistentStore: {};
+var $notify = isQuantumultX ? this.$notify: {};
+var $notification = isSurge ? this.$notification: {};
+if (isQuantumultX) {
+	var errorInfo = {
+		error: ''
+	};
+	$httpClient = {
+		get: (url, cb) = >{
+			var urlObj;
+			if (typeof(url) == 'string') {
+				urlObj = {
+					url: url
+				}
+			} else {
+				urlObj = url
+			}
+			$task.fetch(urlObj).then(response = >{
+				cb(undefined, response, response.body)
+			},
+			reason = >{
+				errorInfo.error = reason.error;
+				cb(errorInfo, response, '')
+			})
+		},
+		post: (url, cb) = >{
+			var urlObj;
+			if (typeof(url) == 'string') {
+				urlObj = {
+					url: url
+				}
+			} else {
+				urlObj = url
+			}
+			url.method = 'POST';
+			$task.fetch(urlObj).then(response = >{
+				cb(undefined, response, response.body)
+			},
+			reason = >{
+				errorInfo.error = reason.error;
+				cb(errorInfo, response, '')
+			})
+		}
+	}
+}
+if (isSurge) {
+	$task = {
+		fetch: url = >{
+			return new Promise((resolve, reject) = >{
+				if (url.method == 'POST') {
+					$httpClient.post(url, (error, response, data) = >{
+						if (response) {
+							response.body = data;
+							resolve(response, {
+								error: error
+							})
+						} else {
+							resolve(null, {
+								error: error
+							})
+						}
+					})
+				} else {
+					$httpClient.get(url, (error, response, data) = >{
+						if (response) {
+							response.body = data;
+							resolve(response, {
+								error: error
+							})
+						} else {
+							resolve(null, {
+								error: error
+							})
+						}
+					})
+				}
+			})
+		}
+	}
+}
+if (isQuantumultX) {
+	$persistentStore = {
+		read: key = >{
+			return $prefs.valueForKey(key)
+		},
+		write: (val, key) = >{
+			return $prefs.setValueForKey(val, key)
+		}
+	}
+}
+if (isSurge) {
+	$prefs = {
+		valueForKey: key = >{
+			return $persistentStore.read(key)
+		},
+		setValueForKey: (val, key) = >{
+			return $persistentStore.write(val, key)
+		}
+	}
+}
+if (isQuantumultX) {
+	$notification = {
+		post: (title, subTitle, detail) = >{
+			$notify(title, subTitle, detail)
+		}
+	}
+}
+if (isSurge) {
+	$notify = function(title, subTitle, detail) {
+		$notification.post(title, subTitle, detail)
+	}
+}
 
 const url = $request.url;
 const limitpath = "https://m-api.nexon.com/sdk/enterToy.nx";
 let body = $response.body;
 let obj = JSON.parse(body);
-if(url == limitpath){
-	obj['errorCode']=0;
-	if(obj.result.service.title.indexOf("Global")> -1){
-		
+if (url == limitpath) {
+	obj['errorCode'] = 0;
+	if (obj.result.service.title.indexOf("Global") > -1) {
+
 		$notify("冒险岛M", "", "国际服锁区解除完成");
-	}
-	else if(obj.result.service.title.indexOf("Japan")>-1){
+	} else if (obj.result.service.title.indexOf("Japan") > -1) {
 		$notify("冒险岛M", "", "日服锁区解除完成");
-	}else{
+	} else {
 		$notify("未知的N社游戏", "", "锁区解除完成");
 	}
 	//console.log(body)
 }
-body = JSON.stringify(obj)
-$done({body:body});
+body = JSON.stringify(obj) $done({
+	body: body
+});
